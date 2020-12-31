6aea3c786a86b4db093cd0f3e73da85506ad85ee
juedoujiang <juedoujiang@qq.com>
2020-12-31 10:01:50 +0800

    OOP_Java

---
+++ src/chapter3/Main.java
@@ -0,0 +1,19 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = "";
+		while(!s.equals("###")) {
+			s = in.next();
+			
+		}
+		
+
+	}
+
+}
